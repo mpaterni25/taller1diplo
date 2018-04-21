2760f65436d214840a801a5c0aa360cad334d563
“Michael” <lordfear19@gmail.com>
2018-04-21 12:47:52 -0500

    Termiando

--- app/src/test/java/com/example/android/taller1/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/taller1/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.example.android.taller1;
 
 import org.junit.Test;
 
+import static com.example.android.taller1.metodos.calculo;
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +12,25 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void prueba10ZapatosMujerClasicoNike() throws Exception {
+        int sexo = 1 , tipo = 1 , marca = 0 , cantidad = 10;
+        assertEquals(600000, calculo(sexo,tipo,marca,cantidad));
     }
-}
+    @Test
+    public void prueba6ZapatosMujerZapatillaAdidas() throws Exception {
+        int sexo = 1 , tipo = 0 , marca = 1 , cantidad = 6;
+        assertEquals(600000, calculo(sexo,tipo,marca,cantidad));
+    }
+    @Test
+    public void prueba8ZapatosHombreZapatillaAdidas() throws Exception {
+        int sexo = 0 , tipo = 0 , marca = 1 , cantidad = 8;
+        assertEquals(1120000, calculo(sexo,tipo,marca,cantidad));
+    }
+    @Test
+    public void prueba4ZapatosHombreClasicoPuma() throws Exception {
+        int sexo = 0 , tipo = 1 , marca = 2 , cantidad = 4;
+        assertEquals(400000, calculo(sexo,tipo,marca,cantidad));
+    }
+
+
+    }
